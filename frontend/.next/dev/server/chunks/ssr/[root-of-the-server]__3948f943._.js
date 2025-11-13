3b81c664caebfb03ac1c126e20d3ec7129cc4d6d
Srikanth Jssr <srikanthborkarjssr@gmail.com>
2025-11-14 03:14:07 +0530

    fix: added missing GlobalLayout component

--- frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__3948f943._.js
+++ frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__3948f943._.js
@@ -81,7 +81,7 @@ __turbopack_context__.s([
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$axios$2f$lib$2f$axios$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/axios/lib/axios.js [app-ssr] (ecmascript)");
 ;
 const api = __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$axios$2f$lib$2f$axios$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"].create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"
+    baseURL: ("TURBOPACK compile-time value", "https://role-based-auth-3.onrender.com") || "http://localhost:5000"
 });
 const __TURBOPACK__default__export__ = api;
 }),
