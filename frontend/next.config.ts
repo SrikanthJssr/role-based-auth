1b03583b9fa7323c0760c49e221ee551ee2d9bd8
Srikanth Jssr <srikanthborkarjssr@gmail.com>
2025-11-14 14:02:08 +0530

    fix: remove turbo key from next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Optional but recommended
+  reactStrictMode: true,
 };
 
 export default nextConfig;
